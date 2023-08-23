0e2b987963365195bc8145e813aef7780d4e400a
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-08-23 13:45:31 -0500

    hello world application commit

---
+++ Hello World/Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Matt Keyzer on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
